dbe382577da97bc108fc097c0ffaddd63bb2869d
Avanishkumarsingh <aksinghmau98@gmail.com>
2023-01-09 16:49:06 +0530

    classes

---
+++ classes/ex2.py
@@ -0,0 +1,15 @@
+class Dog:
+    # init is used to initialize the object
+    def __init__(self, breed, age, weight):
+        self.breed = breed
+        self.age = age
+        self. weight = weight
+        print("Dog object created")
+
+panther = Dog(breed="German Sherpherd", age=11, weight=30)
+tiger = Dog(breed="Pug", age=3, weight= 15)
+blacky = Dog(breed="Labrodor", age= 5, weight= 25)
+
+print(panther.breed)
+print(tiger.breed)
+print(blacky.breed)
