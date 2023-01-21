3fb2bf99e8b5cc5ea511e9fefd689bdb11d3d698
Avanishkumarsingh <aksinghmau98@gmail.com>
2023-01-21 21:12:16 +0530

    analysis

--- classes/g5.py
+++ classes/g5.py
@@ -5,7 +5,7 @@ class Robot(Actor):
     def __init__(self, imglist, **kwargs):
         super().__init__(imglist[0])
         self.imglist = imglist
-        
+
     def walk(self):
         self.image = self.imglist[self.frame]
         print((self.frame + 1) % len(self.imglist))
@@ -15,7 +15,7 @@ class Robot(Actor):
         self.x += self.speed
 
 HEIGHT = 400
-WIDTH = 600
+WIDTH =600
 r = Robot(['w0','w1','w2','w3','w4','w5','w6','w7'],center=(100,100))
 def draw():
     screen.clear()
